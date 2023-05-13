d2d116622cab7d4d21bc8967ad02447b075b22be
hoeen <hoeen5373@gmail.com>
2023-05-13 18:01:09 +0900

    🔥 (코드수정) 실패 : 투포인터에서 조금 생소한 유형이었음. 어떻게 투포인터로 접근할지 잘 생각해서 풀어보기.

--- solution_codes/programmers_투포인터_좋다_1회답.py
+++ solution_codes/programmers_투포인터_좋다_1회답.py
@@ -1,29 +1,8 @@
-from collections import Counter
-
-n = int(input())
-nums = list(map(int, input().split()))
-
-## 시도 1 : Counter 과 set을 이용함 -> 다른 두 수의 합을 고려 못함..
-# good = 0
-
-# sums = set()
-# for i in range(1, n):
-#     for j in range(i):
-#         sums.add(nums[i]+nums[j])
-
-# counter = Counter(nums)
-# print('sums:', sums)
-# print('set_counter:', set(counter.keys()))
-# good_set = sums & set(nums)
-# print('good_set:', good_set)
-# for g in good_set:
-#     good += counter[g]
-# print(good)
-
-
 ## 시도 2 : 투포인터
 # 한 숫자가 다른 두 수로 표현되는지를 한 숫자를 제외한 리스트에서 투포인터로 접근.
 # -5 -5 -4 -3 1 2 2 3
+n = int(input())
+nums = list(map(int, input().split()))
 nums.sort()
 goods = 0
 for i in range(n):
@@ -39,3 +18,24 @@ for i in range(n):
         else:
             left += 1
 print(goods)
+
+
+## 시도 1 : Counter 과 set을 이용함 -> 다른 두 수의 합을 고려 못함..
+# n = int(input())
+# nums = list(map(int, input().split()))
+# from collections import Counter
+# good = 0
+
+# sums = set()
+# for i in range(1, n):
+#     for j in range(i):
+#         sums.add(nums[i]+nums[j])
+
+# counter = Counter(nums)
+# print('sums:', sums)
+# print('set_counter:', set(counter.keys()))
+# good_set = sums & set(nums)
+# print('good_set:', good_set)
+# for g in good_set:
+#     good += counter[g]
+# print(good)
